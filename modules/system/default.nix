0c0f73770dc27be7f7efa4e94c50e9ce3ea9a804
Jelle <jelle.verheyen@pm.me>
2025-04-14 09:22:11 +0200

    fix: Set up fonts with default fallbacks and CJK + Persian support
    See: https://nixos.wiki/wiki/Fonts
    This should make applications render correctly by default

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -7,13 +7,48 @@
     ./ai.nix
   ];
 
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-    liberation_ttf
-    nerd-fonts.iosevka-term
-  ];
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs; [
+      nerd-fonts.iosevka-term
+      liberation_ttf
+      noto-fonts
+      noto-fonts-emoji
+      # CJK
+      noto-fonts-cjk-sans
+      wqy_microhei
+      wqy_zenhei
+
+      # Persian fonts
+      vazir-fonts
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        serif = [
+          "Noto Serif"
+          "Liberation Serif"
+          "Vazirmatn"
+        ];
+        sansSerif = [
+          "Noto Sans"
+          "Noto Sans CJK SC"
+          "Liberation Sans"
+          "Vazirmatn"
+          "Noto Color Emoji"
+        ];
+        monospace = [
+          "Iosevka Term Nerd Font"
+          "Noto Sans Mono CJK SC"
+          "Liberation Mono"
+        ];
+        emoji = [
+          "Noto Color Emoji"
+        ];
+      };
+    };
+  };
+
   hardware.opentabletdriver.enable = true;
   nixpkgs.config.permittedInsecurePackages = [
   ];
